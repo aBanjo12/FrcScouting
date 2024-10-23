78a64624296a88155a10b4da8719bc3c73fd5f65
aBanjo12 <76058655+aBanjo12@users.noreply.github.com>
2024-10-23 15:12:46 -0400

    mess around with openapi gen

--- BlueApiInterface.cs
+++ BlueApiInterface.cs
@@ -1,42 +1,29 @@
 using System.Net.Http.Headers;
 using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
 using Newtonsoft.Json;
+using TBAAPI.V3Client.Api;
+using TBAAPI.V3Client.Client;
+using TBAAPI.V3Client.Model;
 
 namespace FrcScouting;
 
 public static class BlueApiInterface
 {
-    public static HttpClient client = new HttpClient();
+    public static EventApi Event;
+    public static MatchApi Match;
+    public static TeamApi Team;
+    public static DistrictApi District;
+    public static ListApi List;
     static BlueApiInterface()
     {
-        Console.WriteLine(File.ReadAllText("tbakey"));
-        client.DefaultRequestHeaders.Authorization =
-            new AuthenticationHeaderValue("X-TBA-Auth-Key", File.ReadAllText("tbakey"));
-        client.BaseAddress = new Uri("https://www.thebluealliance.com/api/v3/");
-        Console.WriteLine("BlueApiInterface initialized");
-    }
-    
-    private static async Task<ApiRequest<T>> makeRequest<T>(string uri)
-    {
-        Console.WriteLine("requesting " + uri);
-        // Make the GET request
-        HttpResponseMessage response = await client.GetAsync(uri);
+        Configuration config = new Configuration();
+        config.BasePath = "https://www.thebluealliance.com/api/v3";
+        config.ApiKey.Add("X-TBA-Auth-Key", File.ReadAllText("tbakey"));
         
-        if (response.IsSuccessStatusCode)
-        {
-            Console.WriteLine("request for " + typeof(T) + " succeeded");
-            // Read the response content
-            string responseBody = await response.Content.ReadAsStringAsync();
-            return new ApiRequest<T>(JsonConvert.DeserializeObject<T>(responseBody), true);
-        }
-
-        Console.WriteLine("request for " + typeof(T) + " failed");
-        return new ApiRequest<T>(JsonConvert.DeserializeObject<T>(""), false);
-    }
-
-    public static async Task<ApiRequest<string[]>> getTeams(string id)
-    {
-        Console.WriteLine("getting teams for " + id);
-        return await makeRequest<string[]>($"/event/{id}/teams/keys");
+        Event = new EventApi(config);
+        Match = new MatchApi(config);
+        Team = new TeamApi(config);
+        District = new DistrictApi(config);
+        List = new ListApi(config);
     }
 }
--- StatboticsApiInterface.cs
+++ StatboticsApiInterface.cs
@@ -1,48 +1,15 @@
 using FrcScouting.JsonObjects.TeamYear;
 using FrcScouting.JsonObjects.Year;
 using Newtonsoft.Json;
+using Statbotics.Functions;
 
 namespace FrcScouting;
 
 public static class StatboticsApiInterface
 {
-    public static Dictionary<int, Year> years = new Dictionary<int, Year>();
-    public static async Task<ApiRequest<TeamYear>> getTeam(int teamNumber)
+    public static EventApi Event;
+    public static 
+    static StatboticsApiInterface()
     {
-        return await makeRequest<TeamYear>("v3/team_year/" + teamNumber + "/2024");
     }
-    
-    public static async Task<ApiRequest<Year>> getYear(int year)
-    {
-        return await makeRequest<Year>("v3/year/" + year);
-    }
-
-    private static async Task<ApiRequest<T>> makeRequest<T>(string uri)
-    {
-        // Create an instance of HttpClient
-        HttpClient client = new HttpClient();
-
-        // Set the base address (optional)
-        client.BaseAddress = new Uri("https://api.statbotics.io/");
-
-        // Make the GET request
-        HttpResponseMessage response = await client.GetAsync(uri);
-        
-        if (response.IsSuccessStatusCode)
-        {
-            // Read the response content
-            string responseBody = await response.Content.ReadAsStringAsync();
-            return new ApiRequest<T>(JsonConvert.DeserializeObject<T>(responseBody), true);
-        }
-
-        Console.WriteLine("request for " + typeof(T) + " failed");
-        return new ApiRequest<T>(JsonConvert.DeserializeObject<T>(""), false);
-    }
-
-    public static async Task<ApiRequest<TeamYear>> get1014()
-    {
-        return await getTeam(1014);
-    }
-    
-    
 }
